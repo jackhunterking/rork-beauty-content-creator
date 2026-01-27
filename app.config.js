fba14d9dcf047d3ec0e442756b6f20eea6310f92
jackhunterking <metinhakanokuyucu@Mac-Studio.local>
2026-01-27 14:30:58 -0500

    Bump version to 2.1

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
   expo: {
     name: "Resulta",
     slug: "resulta",
-    version: "2.0",
+    version: "2.1",
     orientation: "portrait",
     icon: "./assets/images/resultalogo.png",
     scheme: "resulta",
